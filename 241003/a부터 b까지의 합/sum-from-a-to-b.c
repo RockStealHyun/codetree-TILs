ce1b4a49a8405388473122efa418f65d69903658
RockStealHyun <sksmsdngus0221@gmail.com>
2024-10-03 22:59:53 +0900

    Title: a부터 b까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/a부터 b까지의 합/sum-from-a-to-b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+	int a ,b;
+	int sum_val = 0;
+    scanf("%d %d",&a,&b);
+
+	for (int i = a; i <= b; i++) {
+			sum_val += i;
+		
+	}
+	printf("%d", sum_val);
+	return 0;
+
+}
