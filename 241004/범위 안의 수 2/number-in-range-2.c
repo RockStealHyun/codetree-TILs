59533eb4b8b9c82667b4ebd7c45de006ee70a651
RockStealHyun <sksmsdngus0221@gmail.com>
2024-10-04 10:38:49 +0900

    Title: 범위 안의 수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241004/범위 안의 수 2/number-in-range-2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    // 변수 선언 및 입력
+    int num;
+    int sum = 0, cnt = 0;
+    double avg;
+
+    // 입력을 받으며 0이상 200이하의 정수들을 선별합니다.
+    for(int i = 1; i <= 10; i++) {
+        scanf("%d", &num);
+        if(num >= 0 && num <= 200) {
+            sum += num;
+            cnt++;
+        }
+    }
+
+    // 0이상 200이하의 정수들의 평균을 구합니다.
+    avg = (double)sum / cnt;
+
+    // 출력
+    printf("%d %.1lf", sum, avg);
+
+    return 0;
+}
