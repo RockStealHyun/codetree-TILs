34ce43a966403e8b2e7430d4a4f48bbf203b9a28
RockStealHyun <sksmsdngus0221@gmail.com>
2024-10-06 23:36:01 +0900

    Title: 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241006/3n + 1 수열/3n-plus-1-sequence.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+    int N, cnt = 0;
+    scanf("%d", &N);
+
+    while (true) {
+        if (N % 2 == 0) {  
+            N = N / 2;
+            cnt = cnt + 1;
+
+        }
+        else {  
+            N = N * 3 + 1;
+            cnt = cnt + 1;
+
+        }
+
+        if (N == 1) {  
+
+            break;
+        }
+    }
+
+    printf("%d", cnt);  
+
+    return 0;
+}
