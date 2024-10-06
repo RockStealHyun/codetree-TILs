e249d305f52d7497eefa4b58f5d73013feb4fbe8
RockStealHyun <sksmsdngus0221@gmail.com>
2024-10-06 20:15:00 +0900

    Title: 1까지 나누기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241006/1까지 나누기/divide-by-1.c
+++ 241006/1까지 나누기/divide-by-1.c
@@ -3,24 +3,22 @@
 int main() {
     int n;
     int i;
-    int cnt = 0;
+    int cnt = 0;   // 나눗셈 진행 횟수 저장
 
     // 정수 n 입력 받기
     scanf("%d", &n);
 
-    // 1부터 차례대로 나누기 시작
+    // 1부터 차례대로 n을 나누기
     for (i = 1; i <= n; i++) {
-        int quotient = n / i;  // n을 i로 나눈 몫
+        int quotient = n / i;  // n을 i로 나눈 몫 저장
+        cnt++;  // 나눗셈 진행 횟수 증가
 
-        if (quotient <= 1) {  // 몫이 1 이하일 때 멈추기
-            cnt++;  // 마지막 나눗셈도 포함
+        if (quotient <= 1) {  // 몫이 1 이하가 되면 종료
             break;
         }
-
-        cnt++;  // 나눗셈 횟수 증가
     }
 
-    // 나눗셈이 몇 번 진행되었는지 출력
+    // 총 나눗셈 횟수 출력
     printf("%d\n", cnt);
 
     return 0;
