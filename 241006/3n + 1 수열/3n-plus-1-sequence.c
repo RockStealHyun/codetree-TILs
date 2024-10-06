c5e22df3fdfb73556dac8865ff346f1f1d06c2ec
RockStealHyun <sksmsdngus0221@gmail.com>
2024-10-06 23:40:36 +0900

    Title: 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241006/3n + 1 수열/3n-plus-1-sequence.c
+++ 241006/3n + 1 수열/3n-plus-1-sequence.c
@@ -7,24 +7,24 @@ int main() {
     scanf("%d", &N);
 
     while (true) {
-        if (N % 2 == 0) {  
+        if (N % 2 == 0) {  // N이 짝수일 때
             N = N / 2;
             cnt = cnt + 1;
-
+         
         }
-        else {  
+        else if (N % 2 !=0&&N !=1){  // N이 홀수일 때
             N = N * 3 + 1;
             cnt = cnt + 1;
-
+        
         }
 
-    if (N == 1) {  
-
+        else  {  // N이 1이 되면 종료
+        
             break;
         }
     }
 
-    printf("%d", cnt);  
+    printf("%d", cnt);  // 총 작업 횟수 출력
 
     return 0;
 }
