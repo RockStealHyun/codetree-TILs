6e11cb0ef892a222a5b40b87edfdff119579abfc
RockStealHyun <sksmsdngus0221@gmail.com>
2024-10-07 14:58:01 +0900

    Title: 정사각형 별표 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241007/정사각형 별표 출력하기/print-stars-in-square.c
+++ 241007/정사각형 별표 출력하기/print-stars-in-square.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-	// 변수 선언 및 입력
+
 	int n;
 	scanf("%d", &n);
 
-	// n * n 크기의 별을 출력합니다.
+
 	for(int i = 0; i < n; i++) {
 		for(int j = 0; j < n; j++)
 			printf("*");
