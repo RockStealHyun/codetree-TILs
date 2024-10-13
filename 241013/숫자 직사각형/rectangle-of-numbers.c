62e82d9ad9174de9fd111ffc3bda3bb3916ce979
RockStealHyun <sksmsdngus0221@gmail.com>
2024-10-13 09:16:51 +0900

    Title: 숫자 직사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241013/숫자 직사각형/rectangle-of-numbers.c
+++ 241013/숫자 직사각형/rectangle-of-numbers.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 
 int main() {
-
-    int i, j;
-    int num=0;
-	int arr_2d[4][3];
-
-
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 3; j++) {
-			num += 1;
-            arr_2d[i][j] = num;
-			
-		}
-	}
-
-
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 3; j++) {
+    // 2차원 배열을 구현합니다.
+	int arr_2d[100][100];
+	int cnt = 1;
+	
+	// n과 m을 입력받습니다.
+	int n, m;
+	scanf("%d %d", &n, &m);
+	
+	// n * m 크기의 배열에 숫자를 채워 넣습니다.
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < m; j++)
+			arr_2d[i][j] = cnt++;
+	
+	// 숫자 직사각형을 출력합니다.
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < m; j++)
 			printf("%d ", arr_2d[i][j]);
-		}
 		printf("\n");
 	}
 
-	return 0;
-
+    return 0;
 }
