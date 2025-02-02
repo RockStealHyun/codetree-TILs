73039e59ad0cc621c5285a3c92e32fb53265f6d4
RockStealHyun <sksmsdngus0221@gmail.com>
2025-02-02 23:50:31 +0900

    Title: 건너뛰고 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250202/건너뛰고 별표 출력하기/skip-and-print-a-star.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    // 길이가 n인 직각삼각형을 출력합니다.
+    for(int i = 0; i < n; i++) {
+    for(int j = 0; j <= i; j++)
+    printf("*");
+    printf("\n");
+    printf("\n");
+    }
+	
+    // 길이가 n-1인 직각삼각형을 뒤집어 출력합니다.
+    for(int i = n-2; i >= 0; i--) {
+    for(int j = 0; j <= i; j++)
+    printf("*");
+    printf("\n");
+    printf("\n");
+    }
+
+    return 0;
+}
+
