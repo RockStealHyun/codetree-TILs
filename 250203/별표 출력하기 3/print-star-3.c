034fbf81fb97b59814757413477a00066afa0c08
RockStealHyun <sksmsdngus0221@gmail.com>
2025-02-03 00:39:39 +0900

    Title: 별표 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250203/별표 출력하기 3/print-star-3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+    // 변수 선언 및 입력
+    int n;
+    scanf("%d", &n);
+
+    // 모양에 맞게 별을 출력합니다.
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < i; j++)
+			printf("  ");
+		for(int j = 0; j < (2 * n) - (2 * i) - 1; j++)
+			printf("* ");
+		printf("\n");
+    }
+	
+    return 0;
+}
+
