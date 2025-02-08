4386517fd5e8e233f7aed45419a4ec622a31fe17
RockStealHyun <sksmsdngus0221@gmail.com>
2025-02-08 23:12:21 +0900

    Title: 별표 출력하기 8, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250208/별표 출력하기 8/print-star-8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);  
+
+	for (int i = 1; i <= n; i++) {
+		if (i % 2 == 0) {
+			for (int j = 0; j < i ; j++) {
+				printf("* ");
+			}
+		}
+		else {
+			printf("* ");
+		}
+		printf("\n");
+	}
+
+	return 0;
+	
+}
+
