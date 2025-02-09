d1704cb1bec2471a096633eb2bfbcb8d9d14a13d
RockStealHyun <sksmsdngus0221@gmail.com>
2025-02-09 22:56:31 +0900

    Title: 1-9 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250209/1-9 정사각형/1-9-square.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+	int cnt = 1;
+	
+
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			printf("%d", cnt);
+			cnt++;
+			if(cnt == 10)
+				cnt = 1;
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
