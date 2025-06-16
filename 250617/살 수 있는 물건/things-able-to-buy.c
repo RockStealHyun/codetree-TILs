9f4c3c334116ece738c16448d467046f3618e4f9
RockStealHyun <sksmsdngus0221@gmail.com>
2025-06-17 00:26:00 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250617/살 수 있는 물건/things-able-to-buy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    if (N>=3000) {
+    printf("mask");
+    }
+    else if (N<3000 && N>=1000) {
+    printf("mask");
+    }
+    else {
+    printf("mask");
+    }
+
+
+    return 0;
+}
+
