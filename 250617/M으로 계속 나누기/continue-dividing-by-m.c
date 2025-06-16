4604451d6c25969e944792f2b7a9003b975e7e11
RockStealHyun <sksmsdngus0221@gmail.com>
2025-06-17 03:40:02 +0900

    Title: M으로 계속 나누기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250617/M으로 계속 나누기/continue-dividing-by-m.c
+++ 250617/M으로 계속 나누기/continue-dividing-by-m.c
@@ -1,17 +1,13 @@
-#include <iostream>
-
-using namespace std;
+#include <stdio.h>
 
 int main() {
     int N, M;
+    scanf("%d %d", &N, &M);  // N과 M 입력 받기
 
-    cin >> N;
-    cin >> M;
-    
-    while(N > 0) {
-        cout << N << "\n";
-        N /= M;
+    while (N > 0) {
+        printf("%d\n", N);   // 현재 N 출력
+        N = N / M;           // N을 M으로 나눈 몫으로 갱신
     }
-    
+
     return 0;
 }
