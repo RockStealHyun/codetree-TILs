fa90c8336ed58ea5b8afaf0ba4f097c5c74ae1fa
RockStealHyun <sksmsdngus0221@gmail.com>
2025-06-16 14:49:56 +0900

    Title: 사칙연산 연습, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250616/사칙연산 연습/practice-basic-arithmetic-operations.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 30;
+    int c;
+    
+    c=a+b;
+
+    printf("%d\n", c / 8);  // 출력: 3
+    printf("%d\n", c % 8);  // 출력: 3
+    return 0;
+}
+
