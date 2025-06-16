ea68e6daebbef3e6eb4571108d9a0847c845b3f5
RockStealHyun <sksmsdngus0221@gmail.com>
2025-06-16 22:16:24 +0900

    Title: 음수 구별하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250616/음수 구별하기/separate-negative-number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d",&N);
+    printf("%d",N)
+
+    if(N <= -1){
+        printf("minus");
+    }
+    return 0;
+}
+
+
+
+//저는 살면서 한번도 술을 마셔본 적이 없습니다. 
