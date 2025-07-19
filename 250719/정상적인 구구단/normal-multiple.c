aadc0c81300e087b4e888abdd92c1ae03e41c6f2
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-19 20:55:10 +0900

    Title: 정상적인 구구단, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250719/정상적인 구구단/normal-multiple.c
+++ 250719/정상적인 구구단/normal-multiple.c
@@ -1,15 +1,19 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
-    int N;
-    scanf("%d",&N);
-
-    for(int i = 1; i <= N; i++) {
-        for(int j = 1; j <= N; j++) {
-            printf("%d * %d = %d, ", i, j, i * j);
-        }
-        printf("\n");
-    }
-
-    return 0;
+    // 변수 선언 및 입력
+    int n;
+    scanf("%d", &n);
+	
+	// n*n모양 구구단을 출력합니다.
+	for(int i = 1; i <= n; i++) {
+		for(int j = 1; j <= n; j++) {
+			printf("%d * %d = %d", i, j, i * j);
+			if(j != n)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	
+	return 0;
 }
