b6eb99f6426c62d1070447d1b60dc453e8bcb8b8
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-19 23:16:03 +0900

    Title: A 부터 B 까지, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250719/A 부터 B 까지/a-to-b.c
+++ 250719/A 부터 B 까지/a-to-b.c
@@ -4,14 +4,17 @@ int main() {
     int A, B;
     scanf("%d %d", &A, &B);
 
-    while (A < B) {
-        printf("%d ", A);
+    while (A <= B) {
+        printf("%d ", A);  // 출력 먼저 하고
 
+        // 다음 A 값을 계산
         if (A % 2 == 1) {
-            A *= 2;       // 홀수면 2배
+            A *= 2;
         } else {
-            A += 3;       // 짝수면 +3
+            A += 3;
         }
+
+        // 근데 다음 A가 B보다 커지면 다음 루프에서 종료됨
     }
 
     return 0;
