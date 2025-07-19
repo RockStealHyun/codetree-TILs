6115e327e31e0d387edda0ad38a2e28b130fbeb3
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-20 02:33:08 +0900

    Title: 배열의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250720/배열의 합/sum-of-array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int i,j;
+    int seat;
+
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            scanf("%d",&seat[i][j]) {
+            }
+        }
+    }
+
+    printf("%d개\n", cnt);
+    return 0;
+}
