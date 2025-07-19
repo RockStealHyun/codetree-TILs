168194f4dfbf93235b0d5a7fb60c7a2f8023beec
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-19 21:09:16 +0900

    Title: 숫자 피라미드, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250719/숫자 피라미드/number-pyramid.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+    // 변수 선언 및 입력
+    int n;
+    scanf("%d", &n);
+	
+	// n*n모양 구구단을 출력합니다.
+	for(int i = 1; i <= n; i++) {
+		for(int j = 1; j <= i; j++) {
+			printf("%d ",i);
+
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
