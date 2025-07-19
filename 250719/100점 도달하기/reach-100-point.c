d8aa4391a7634a0b3e554b20dce2793c30688635
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-19 14:37:40 +0900

    Title: 100점 도달하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250719/100점 도달하기/reach-100-point.c
+++ 250719/100점 도달하기/reach-100-point.c
@@ -3,23 +3,23 @@
 
 int main() {
     int N;
-    scanf("%d", N);  
+    scanf("%d", &N);  
     
     for(int i = N; i < 101; i++){
-        if(N<60){
-            printf("F");
+        if(i<60){
+            printf("F ");
         }
-        else if(N>=60&&N<70){
-            printf("D");
+        else if(i>=60&&i<70){
+            printf("D ");
         }
-        else if(N>=70&&N<80){
-            printf("C");
+        else if(i>=70&&i<80){
+            printf("C ");
         }
-        else if(N>=80&&N<90){
-            printf("B");
+        else if(i>=80&&i<90){
+            printf("B ");
         }
-        else if(N>=90&&N<101){
-            printf("A");
+        else if(i>=90&&i<101){
+            printf("A ");
         }
 
 
