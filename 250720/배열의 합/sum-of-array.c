d98156bc33296c28adbfafb5a74afce793dd982a
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-20 02:33:26 +0900

    Title: 배열의 합, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250720/배열의 합/sum-of-array.c
+++ 250720/배열의 합/sum-of-array.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
 int main() {
+    // 2차원 배열을 구현합니다.
+	int arr_2d[4][4];
+	
+	// 각 줄마다 정수를 입력받습니다.
+	for(int i = 0; i < 4; i++)
+		for(int j = 0; j < 4; j++)
+			scanf("%d", &arr_2d[i][j]);
+	
+	// 각 줄의 합을 출력합니다.
+	for(int i = 0; i < 4; i++) {
+		int sum = 0;
+		for(int j = 0; j < 4; j++)
+			sum += arr_2d[i][j];
+		printf("%d\n", sum);
+	}
 
-    int i,j;
-    int seat;
-
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
-            scanf("%d",&seat[i][j]) {
-            }
-        }
-    }
-
-    printf("%d개\n", cnt);
     return 0;
 }
