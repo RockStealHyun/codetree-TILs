cdbd1a13042e446a5606a0aaa1ef7a120936f3fc
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-20 22:13:14 +0900

    Title: 최대공약수 구하기, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250720/최대공약수 구하기/find-the-greatest-common-divisor.c
+++ 250720/최대공약수 구하기/find-the-greatest-common-divisor.c
@@ -1,25 +1,23 @@
-#include <iostream>
-#include <algorithm>
+#include <stdio.h>
 
-using namespace std;
-
-// n과 m의 최대공약수를 출력합니다.
-void FindGcd(int n, int m) {
-    int gcd = 0;
-    for(int i = 1; i <= min(n, m); i++) {
-        if(n % i == 0 && m % i == 0)
-            gcd = i;
+// N×N 크기의 숫자 사각형을 출력하는 함수
+// 1부터 시작해서 9 다음에는 다시 1로 돌아갑니다.
+void printSquare(int N) {
+    for (int i = 0; i < N; i++) {         // 각 행을 반복
+        for (int j = 0; j < N; j++) {     // 한 행 안에서 N개의 숫자를 출력
+            int idx = i * N + j;          // 0부터 시작하는 인덱스
+            int num = (idx % 9) + 1;      // 1~9 반복하도록 계산
+            printf("%d ", num);           // 숫자 출력 뒤에 공백
+        }
+        printf("\n");                     // 한 행을 다 출력하면 줄바꿈
     }
-
-    cout << gcd;
 }
 
-int main() {
-    // 변수 선언 및 입력:
-    int n, m;
-    cin >> n >> m;
-
-    FindGcd(n, m);
-    
+int main(void) {
+    int N;
+    // 정수 하나 입력받기
+    scanf("%d", &N);
+    // 함수를 호출해 사각형 출력
+    printSquare(N);
     return 0;
 }
