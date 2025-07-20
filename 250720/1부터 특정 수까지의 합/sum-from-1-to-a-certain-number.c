589710216304828a57d8ba648bc682bac44dfd56
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-20 22:33:11 +0900

    Title: 1부터 특정 수까지의 합, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250720/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.c
+++ 250720/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
-// 1부터 N까지 더한 합을 10으로 나눈 나머지를 반환하는 함수
-int sumMod10(int N) {
+// 1부터 N까지 합한 뒤 10으로 나눈 몫을 반환하는 함수
+int sumQuotientBy10(int N) {
     int sum = 0;
     for (int i = 1; i <= N; i++) {
-        sum += i;         // 1부터 i까지 계속 더함
+        sum += i;            // 1부터 i까지 계속 더하기
     }
-    return sum % 10;      // 10으로 나눈 나머지 반환
+    return sum / 10;         // 10으로 나눈 몫만 반환 (나머지 버림)
 }
 
 int main(void) {
     int N;
-    // 사용자로부터 정수 하나 입력받기
-    scanf("%d", &N);
-
-    // 함수 호출해서 결과 계산 후 출력
-    printf("%d\n", sumMod10(N));
+    scanf("%d", &N);                             // 정수 하나 입력
+    printf("%d\n", sumQuotientBy10(N));          // 함수 호출 후 결과 출력
     return 0;
 }
