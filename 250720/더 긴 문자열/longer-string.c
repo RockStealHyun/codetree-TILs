4974ba3918c1718d67161299784e767705f1a209
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-20 13:02:27 +0900

    Title: 더 긴 문자열, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250720/더 긴 문자열/longer-string.c
+++ 250720/더 긴 문자열/longer-string.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main(void) {
+    char str1[102], str2[102];
+    scanf("%s %s", str1, str2);
 
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
 
-	char str1[102];
-	char str2[102];
-	scanf("%s", str1);
-	scanf("%s", str2);
+    if (len1 == len2) {
+        printf("same");
+    } else if (len1 > len2) {
+        printf("%s %d", str1, len1);
+    } else {
+        printf("%s %d", str2, len2);
+    }
 
-	if(strlen(str1)== strlen(str2)); 
-	    printf("same");
-    else if
-        if(strlen(str1) > strlen(str2))
-            printf("%s %d",str1,strlen(str1));
-        else if(strlen(str1) < strlen(str2))
-            printf("%s %d",str2,strlen(str2));
-
-	return 0;
+    return 0;
 }
-
-
