76040ba5442311f0761459e323a20bf4f82c436d
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-20 14:57:55 +0900

    Title: 알파벳 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250720/알파벳 출력하기 2/print-alphabet-2.c
+++ 250720/알파벳 출력하기 2/print-alphabet-2.c
@@ -6,19 +6,18 @@ int main(void) {
 
     char ch = 'A';
     for (int i = 0; i < N; i++) {
-        // 1) i칸 들여쓰기
-        for (int s = 0; s < i; s++) {
+        // 2*i칸 들여쓰기 (오른쪽 정렬)
+        for (int s = 0; s < i * 2; s++) {
             putchar(' ');
         }
-        // 2) N-i개 알파벳 출력 (사이사이에 한 칸 공백)
+        // N–i글자 출력 (글자 사이에 한 칸 공백)
         for (int j = 0; j < N - i; j++) {
-            if (j > 0) putchar(' ');
             putchar(ch);
+            if (j < N - i - 1) putchar(' ');
             ch++;
             if (ch > 'Z') ch = 'A';
         }
         putchar('\n');
     }
-
     return 0;
 }
