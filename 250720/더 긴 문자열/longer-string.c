869b96f74af04b1926a0afa0045683e785d07b7b
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-20 13:01:55 +0900

    Title: 더 긴 문자열, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250720/더 긴 문자열/longer-string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+
+	char str1[102];
+	char str2[102];
+	scanf("%s", str1);
+	scanf("%s", str2);
+
+	if(strlen(str1)== strlen(str2)); 
+	    printf("same");
+    else if
+        if(strlen(str1) > strlen(str2))
+            printf("%s %d",str1,strlen(str1));
+        else if(strlen(str1) < strlen(str2))
+            printf("%s %d",str2,strlen(str2));
+
+	return 0;
+}
+
+
