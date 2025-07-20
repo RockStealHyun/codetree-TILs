b5e271ee225ad4110df9b6f9b1b39627c880d2e7
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-20 15:33:08 +0900

    Title: 격자에 점 그리기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250720/격자에 점 그리기/draw-points-on-grid.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // 2차원 배열을 구현합니다.
+	int arr[10][10] = {0,};
+	
+	// n, m을 입력받습니다.
+	int n, m;
+	scanf("%d %d", &n, &m);
+	
+	// m회에 걸쳐 동전의 위치를 입력받고 올바른 위치에 입력된 순서를 표기합니다.
+	for(int i = 1; i <= m; i++) {
+		int r, c;
+		scanf("%d %d", &r, &c);
+		arr[r][c] = i;
+	}
+			
+	// 채워진 배열을 출력합니다.
+	for(int i = 1; i <= n; i++) {
+		for(int j = 1; j <= n; j++)
+			printf("%d ", arr[i][j]);
+		printf("\n");
+	}
+
+    return 0;
+}
