ba3ac62298eab36db04abbe35b0b953e21532b98
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-20 14:47:11 +0900

    Title: 알파벳 출력하기 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250720/알파벳 출력하기 2/print-alphabet-2.c
+++ 250720/알파벳 출력하기 2/print-alphabet-2.c
@@ -4,17 +4,20 @@ int main(void) {
     int N;
     scanf("%d", &N);
 
-    char ch = 'A';  // 시작 문자
-
+    char ch = 'A';  // 알파벳 시작
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%c ", ch);
-            ch++;
-            if (ch > 'Z') {
-                ch = 'A';  // Z 다음엔 다시 A로
-            }
+        printf("%c", ch);
+
+        if ((i + 1) % 3 == 0 || i == N - 1) {
+            printf("\n");
+        } else {
+            printf(" ");
         }
-        printf("\n");  // 한 줄 끝
+
+        ch++;
+        if (ch > 'Z') {
+            ch = 'A';
+        }
     }
 
     return 0;
