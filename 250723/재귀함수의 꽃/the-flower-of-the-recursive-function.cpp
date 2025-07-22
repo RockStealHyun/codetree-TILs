0afffbc950b75da2fbe84655f3d1e9c4ed424469
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-23 00:49:43 +0900

    Title: 재귀함수의 꽃, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250723/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
+++ 250723/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -2,12 +2,24 @@
 
 using namespace std;
 
-int N;
+
+// 재귀함수를 이용해 숫자를 n부터 1까지 출력하고, 다시 1부터 n까지 출력합니다.
+void PrintStar(int n) {
+    if(n == 0)
+        return;
+
+    cout << n << " ";
+    PrintStar(n - 1);
+    cout << n << " ";
+}
+
 
 int main() {
-    cin >> N;
+    // 변수 선언 및 입력:
+    int n;
+    cin >> n;
 
-    // Please write your code here.
+    PrintStar(n);
 
     return 0;
 }
