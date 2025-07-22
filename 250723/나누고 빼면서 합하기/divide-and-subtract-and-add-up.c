77d47b6fa942934f3517a07757b499671f95fd14
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-23 00:31:51 +0900

    Title: 나누고 빼면서 합하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250723/나누고 빼면서 합하기/divide-and-subtract-and-add-up.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int arr[101];
+int cnt;
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &arr[i]);
+    cnt = m;
+    // Please write your code here.
+    return 0;
+}
