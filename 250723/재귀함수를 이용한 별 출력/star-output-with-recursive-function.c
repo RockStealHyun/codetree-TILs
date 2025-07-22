97320e3c238f79c6f69973e9e661fd47c3c55247
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-23 00:46:46 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250723/재귀함수를 이용한 별 출력/star-output-with-recursive-function.c
+++ 250723/재귀함수를 이용한 별 출력/star-output-with-recursive-function.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-void print_num(int n) {          // 한 번의 재귀로 내려갔다가 올라오기
-    printf("%d ", n);            // 내려가기 출력
-    if (n == 1) return;
-    print_num(n - 1);            // 더 작은 수로 재귀
-    printf("%d ", n);            // 올라오면서 다시 출력
+void print_tri(int n) {        // 1줄부터 n줄까지 별 출력
+    if (n == 0) return;
+    print_tri(n - 1);          // 먼저 위쪽 출력
+    for (int i = 0; i < n; i++) putchar('*');
+    putchar('\n');
 }
 
 int main(void) {
     int N;
     scanf("%d", &N);
-    print_num(N);
+    print_tri(N);
     return 0;
 }
