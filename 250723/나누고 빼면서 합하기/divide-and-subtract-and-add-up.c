0154ccb6ad4994525a09d1c5a5953ec257eb1099
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-23 00:35:38 +0900

    Title: 나누고 빼면서 합하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250723/나누고 빼면서 합하기/divide-and-subtract-and-add-up.c
+++ 250723/나누고 빼면서 합하기/divide-and-subtract-and-add-up.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
-int arr[101];
-int cnt;
+int next_m(int m) {                 // 다음 M 계산
+    return (m % 2 == 0) ? m / 2 : m - 1;
+}
 
-int main() {
-    int n, m;
-    scanf("%d %d", &n, &m);
-    for (int i = 1; i <= n; i++)
-        scanf("%d", &arr[i]);
-    cnt = m;
-    // Please write your code here.
+int solve(int A[], int M) {         // M번째 원소들을 더함
+    int sum = 0;
+    while (1) {
+        sum += A[M];                // 1-based
+        if (M == 1) break;
+        M = next_m(M);
+    }
+    return sum;
+}
+
+int main(void) {
+    int N, M, A[101];
+    scanf("%d %d", &N, &M);
+    for (int i = 1; i <= N; i++) scanf("%d", &A[i]);
+
+    printf("%d", solve(A, M));
     return 0;
 }
