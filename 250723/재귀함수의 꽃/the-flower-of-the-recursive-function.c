81ac7af83e340af769f667b45d90faed2ad857d3
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-23 00:47:27 +0900

    Title: 재귀함수의 꽃, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250723/재귀함수의 꽃/the-flower-of-the-recursive-function.c
+++ 250723/재귀함수의 꽃/the-flower-of-the-recursive-function.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-void print_num(int n) {          // 한 번의 재귀로 내려갔다가 올라오기
-    printf("%d ", n);            // 내려가기 출력
+void f(int n) {              // 재귀함수 1개만 사용
+    printf("%d ", n);        // 내려가며 출력
     if (n == 1) return;
-    print_num(n - 1);            // 더 작은 수로 재귀
-    printf("%d ", n);            // 올라오면서 다시 출력
+    f(n - 1);                // 더 작은 값으로 재귀
+    printf("%d ", n);        // 올라오며 다시 출력
 }
 
 int main(void) {
     int N;
     scanf("%d", &N);
-    print_num(N);
+    f(N);
     return 0;
 }
