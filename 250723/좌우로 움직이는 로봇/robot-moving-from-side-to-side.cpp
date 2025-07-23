8952dc5de8d98eb2b95b498637f8d7bdc83ceaff
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-23 13:31:48 +0900

    Title: 좌우로 움직이는 로봇, Time: 47ms, Memory: 8MB, Status: Passed - Codetree

--- 250723/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
+++ 250723/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -1,68 +1,73 @@
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
 
-#define MAX 100000
+#define MAX_T 1000000
 
-char a_dir[MAX], b_dir[MAX];
-int a_time[MAX], b_time[MAX];
-int a_pos[MAX + 1], b_pos[MAX + 1];
+using namespace std;
+
+int n, m;
+int pos_a[MAX_T  + 1], pos_b[MAX_T + 1];
 
 int main() {
-    int N, M;
-    scanf("%d %d", &N, &M);
-
-    int total_time = 0;
-    for (int i = 0; i < N; ++i) {
-        int t;
-        char d;
-        scanf("%d %c", &t, &d);
-        a_time[i] = t;
-        a_dir[i] = d;
-        total_time += t;
-    }
-
-    for (int i = 0; i < M; ++i) {
-        int t;
-        char d;
-        scanf("%d %c", &t, &d);
-        b_time[i] = t;
-        b_dir[i] = d;
-    }
-
-    // A의 이동 경로 시뮬레이션
-    int a_cur = 0, a_idx = 0, a_t = 0;
-    for (int t = 1; t <= total_time; ++t) {
-        if (a_t == 0) {
-            a_t = a_time[a_idx];
-        }
-        if (a_dir[a_idx] == 'L') a_cur--;
-        else a_cur++;
-        a_pos[t] = a_cur;
-        a_t--;
-        if (a_t == 0) a_idx++;
-    }
-
-    // B의 이동 경로 시뮬레이션
-    int b_cur = 0, b_idx = 0, b_t = 0;
-    for (int t = 1; t <= total_time; ++t) {
-        if (b_t == 0) {
-            b_t = b_time[b_idx];
-        }
-        if (b_dir[b_idx] == 'L') b_cur--;
-        else b_cur++;
-        b_pos[t] = b_cur;
-        b_t--;
-        if (b_t == 0) b_idx++;
-    }
-
-    // 만나는 횟수 계산
-    int count = 0;
-    for (int t = 1; t <= total_time; ++t) {
-        if (a_pos[t] == b_pos[t] && a_pos[t - 1] != b_pos[t - 1]) {
-            count++;
+    // 입력
+    cin >> n >> m;
+    
+    // A가 매 초마다 서있는 위치를 기록
+    int time_a = 1;
+    for(int i = 0; i < n; i++) {
+        char d; int t;
+        cin >> t >> d;
+        
+        while(t--) {
+            if(d == 'R')
+                pos_a[time_a] = pos_a[time_a - 1] + 1;
+            else
+                pos_a[time_a] = pos_a[time_a - 1] - 1;
+            
+            time_a++;
         }
     }
-
-    printf("%d\n", count);
+    
+    // B가 매 초마다 서있는 위치를 기록
+    int time_b = 1;
+    for(int i = 0; i < m; i++) {
+        int t; char d;
+        cin >> t >> d;
+        
+        while(t--) {
+            if(d == 'R')
+                pos_b[time_b] = pos_b[time_b - 1] + 1;
+            else
+                pos_b[time_b] = pos_b[time_b - 1] - 1;
+            
+            time_b++;
+        }
+    }
+	
+	
+	if(time_a < time_b) {
+		for(int i = time_a; i < time_b; i++) {
+			pos_a[i] = pos_a[i - 1];
+		}
+	}
+	else if(time_a > time_b) {
+		for(int i = time_b; i < time_a; i++) {
+			pos_b[i] = pos_b[i - 1];
+		}
+	}
+    
+    // 새롭게 만나는 횟수를 구합니다.
+    int cnt = 0;
+	int time_max = 0;
+	if(time_a < time_b)
+		time_max = time_b;
+	else
+		time_max = time_a;
+	
+    for(int i = 1; i < time_max; i++) {
+        if(pos_a[i] == pos_b[i] && pos_a[i - 1] != pos_b[i - 1])
+            cnt++;
+    }
+    
+    cout << cnt;
     return 0;
 }
