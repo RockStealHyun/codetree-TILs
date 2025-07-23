b38c4fcf352bbe798c574813d2a0b5461166707f
RockStealHyun <sksmsdngus0221@gmail.com>
2025-07-23 13:30:53 +0900

    Title: 좌우로 움직이는 로봇, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250723/좌우로 움직이는 로봇/robot-moving-from-side-to-side.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    int t;
+    char d;
+    int i;
+    
+    scanf("%d %d", &n, &m);
+    
+    for(i = 0; i < n; i++) {
+        scanf("%d %c", &t, &d);
+    }
+    
+    for(i = 0; i < m; i++) {
+        scanf("%d %c", &t, &d);
+    }
+    
+    // Please write your code here.
+    
+    return 0;
+}
